cb426576b04c379b7cd013e455a161c0b7af58a6
z <phxv@mail.ru>
2018-11-05 00:22:07 +0300

    1

---
+++ data_processing/split_dataset.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import random
+
+
+src_dir = '/w/WORK/ineru/06_scales/_dataset/copy/'
+dst_dir = '/w/WORK/ineru/06_scales/_dataset/splited/'
+parts = ['train', 'valid', 'test']
+
+
+def copy_files_to_subdirs(src_dir, dst_dir, parts, ratio=[9,1,0]):
+
+	src_dir = src_dir.rstrip('/')
+	dst_dir = dst_dir.rstrip('/')
+
+	os.system('mkdir -p {}'.format(dst_dir))	
+	for p in parts:
+		os.system('mkdir -p {}'.format(dst_dir + '/' + p))
+
+	subdirs = os.listdir(src_dir)
+	for class_name in subdirs:
+		subdir = src_dir + '/' + class_name
+		if not os.path.isdir(subdir): continue
+
+		file_names = os.listdir(subdir)
+		if len(file_names) == 0: 
+			print('{0} - empty subdir'.format(class_name))
+			continue
+		
+		# calculate train, valid and test sizes
+		num_files = len(file_names)
+		num_valid = num_files * ratio[1] // sum(ratio)
+		num_test  = num_files * ratio[2] // sum(ratio)
+		num_train = num_files - num_valid - num_test
+		min_num_train = 5
+		if num_train < min_num_train:
+			(num_train, num_valid, num_test) = (num_files, 0, 0)
+
+		random.shuffle(file_names)
+		files = dict()
+		files['train'] = file_names[ : num_train]
+		files['valid'] = file_names[num_train : num_train + num_valid]
+		files['test']  = file_names[num_train + num_valid : ]
+		print(num_files, num_train, num_valid, num_test)
+		print('train:valid:test = ', len(files['train']),\
+			len(files['valid']), len(files['test']))
+
+		for part in parts:
+			cmd = 'mkdir -p {}'.format(dst_dir + '/' + part + '/' + class_name)
+			os.system(cmd)
+			#print(cmd)
+			for file_name in files[part]:
+				src_path = subdir + '/' + file_name
+				dst_path = dst_dir + '/' + part + '/' + class_name + '/' + file_name
+				cmd = 'cp {} {}'.format(src_path, dst_path)
+				os.system(cmd)
+				#print(cmd)
+
+if __name__ == '__main__':
+
+	copy_files_to_subdirs(src_dir, dst_dir, parts, ratio=[9,1,0])
+
