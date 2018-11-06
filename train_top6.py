7dc569dcbf28c8ce0073cf3df65c5965c9c4fa21
z <phxv@mail.ru>
2018-11-06 22:49:11 +0300

    1

--- train_top6.py
+++ train_top6.py
@@ -97,7 +97,7 @@ def accuracy_topk(outputs, labels, k=1):
 	batch_size = len(outputs)
 	res = np.zeros(batch_size, dtype=int)
 	for i in range(batch_size):
-		output = outputs[i].detach().numpy()
+		output = outputs[i].detach().cpu().numpy()
 		label = int(labels[i])
 		#predict = np.argmax(output)
 		topk_predicts = set(output.argsort()[::-1][:k])
