81aa9a6ec14c577dd4c2691402fe163ec311f505
z <phxv@mail.ru>
2018-11-07 11:21:31 +0300

    1

--- train_top6.py
+++ train_top6.py
@@ -186,7 +186,7 @@ optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
 model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-	num_epochs=1)	
+	num_epochs=10)	
 
 # save model
 
